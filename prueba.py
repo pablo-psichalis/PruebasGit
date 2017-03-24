75316cf806b93eccdb9b406c1bac8237ba015f8b
pablo-psichalis <pablo.psrz@gmail.com>
2017-03-24 21:28:48 +0100

    Empecé la funcioncita (B)

--- prueba.py
+++ prueba.py
@@ -5,3 +5,9 @@ print("Programacion basura!")
 
 # Commit 4 (B)
 print("Pero yo estaba trabajando en remoto también ;-;")
+
+# Commit 6 (B)
+print("Por aquí yo seguía trabajando en remoto ;-;")
+
+def cosas():
+	# Ya se sigue mañana 
